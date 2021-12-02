fd9901630ac0532c9204be0008af578aed297b9e
travel.liu <traveldba@gmail.com>
2021-12-02 19:50:53 +0800

    feat: update api sql type

--- api/api_windows.go
+++ api/api_windows.go
@@ -46,12 +46,13 @@ const (
 	SQL_DATETIME        = 9
 	SQL_DATE            = 9
 	SQL_TIME            = 10
+	SQL_TIMESTAMP       = 11
 	SQL_VARCHAR         = 12
+	SQL_BOOLEAN         = 16
 	SQL_TYPE_DATE       = 91
 	SQL_TYPE_TIME       = 92
 	SQL_TYPE_TIMESTAMP  = 93
 	SQL_NEED_DATA       = 99
-	SQL_TIMESTAMP       = 11
 	SQL_LONGVARCHAR     = -1
 	SQL_BINARY          = -2
 	SQL_VARBINARY       = -3
@@ -66,11 +67,12 @@ const (
 	SQL_SIGNED_OFFSET   = -20
 	SQL_UNSIGNED_OFFSET = -22
 	SQL_GRAPHIC         = -95
+	SQL_VARGRAPHIC      = -96
+	SQL_LONGVARGRAPHIC  = -97
 	SQL_BLOB            = -98
 	SQL_CLOB            = -99
 	SQL_DBCLOB          = -350
 	SQL_SS_XML          = -152
-	SQL_BOOLEAN         = 16
 	SQL_DECFLOAT        = -360
 	SQL_XML             = -370
 
